40a1f82210af860e78d64c0dc3fd44517a941c0d
Developer Sid <142804820+Siddarth-abcs@users.noreply.github.com>
2023-10-17 20:42:04 +0530

    done

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+// Get the email address from the second HTML and display it in the first HTML
+// btn.onclick = () => {
+//       let x = document.getElementById("text").value;
+//       alert(x)
+//     };
+    
